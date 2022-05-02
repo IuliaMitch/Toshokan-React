1a41752baf4906eabcc281a505832a20ca7c23f4
IuliaMitch <noipasz@gmail.com>
2022-05-02 17:01:07 -0300

    Criando Modal

---
+++ src/components/Overlay/Overlay.jsx
@@ -0,0 +1,12 @@
+import './Overlay.css'
+
+const Overlay = ({ children, overlayClick}) => {
+    return (
+        <div className="Overlay" onClick={() => overlayClick()}>
+            {children}
+        </div>
+    )
+}
+
+
+export default Overlay
